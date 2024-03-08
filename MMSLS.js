7d9cc6215b92f531138d94ade089d6fc4c5c98a2
Fruuuit <89231189+Fruuuit@users.noreply.github.com>
2024-03-08 14:43:25 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=33605299&pid=71a93b75a4a016ad4ef6b8243b89d7a1&tid=824418328057b66c9bf177f4f88f3936",
+    "https://www.maimemo.com/share/page?uid=33605299&pid=54c96370dff75dffbfb115b8f945f5fd&tid=9ea77e59cdb5981814a035afb19e4e52",
 
 
   ]
